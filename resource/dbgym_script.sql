da80355b0ac5bcd8602818d719d25010888cd007
CJ Bunyi <cjb.nyi@gmail.com>
2024-11-15 22:40:05 +0800

    Revamp DB App structure

--- resource/dbgym_script.sql
+++ resource/dbgym_script.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `dbgym`.`members` (
   `last_name` VARCHAR(45) NOT NULL,
   `first_name` VARCHAR(45) NOT NULL,
   `birthdate` DATE NOT NULL,
-  `sex` ENUM('M', 'F', '-') NULL,
+  `sex` ENUM('M', 'F', '-') NOT NULL,
   `phone_number` CHAR(11) NOT NULL,
   `street` VARCHAR(45) NOT NULL,
   `barangay` VARCHAR(45) NOT NULL,
@@ -33,13 +33,39 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `dbgym`.`membership_types`
+-- Table `dbgym`.`subscription_types`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbgym`.`membership_types` (
-  `membership_type_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `membership_type_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`membership_type_id`),
-  UNIQUE INDEX `membership_type_UNIQUE` (`membership_type_name` ASC) VISIBLE)
+CREATE TABLE IF NOT EXISTS `dbgym`.`subscription_types` (
+  `subscription_type_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `subscription_type_name` VARCHAR(45) NOT NULL,
+  `price` DECIMAL(10,2) UNSIGNED NOT NULL,
+  PRIMARY KEY (`subscription_type_id`),
+  UNIQUE INDEX `membership_type_UNIQUE` (`subscription_type_name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbgym`.`subscriptions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbgym`.`subscriptions` (
+  `subscription_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `member_id` INT UNSIGNED NOT NULL,
+  `subscription_type_id` INT UNSIGNED NOT NULL,
+  `subscription_start_date` DATE NOT NULL,
+  `subscription_end_date` DATE NOT NULL,
+  PRIMARY KEY (`subscription_id`),
+  INDEX `fk_subscriptions_subscription_types1_idx` (`subscription_type_id` ASC) VISIBLE,
+  INDEX `fk_subscriptions_members1_idx` (`member_id` ASC) VISIBLE,
+  CONSTRAINT `fk_subscriptions_subscription_types1`
+    FOREIGN KEY (`subscription_type_id`)
+    REFERENCES `dbgym`.`subscription_types` (`subscription_type_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_subscriptions_members1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `dbgym`.`members` (`member_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -57,44 +83,10 @@ CREATE TABLE IF NOT EXISTS `dbgym`.`trainers` (
   `barangay` VARCHAR(45) NOT NULL,
   `city` VARCHAR(45) NOT NULL,
   `province` VARCHAR(45) NULL,
-  `program_specialty` VARCHAR(45) NOT NULL,
-  `availability_schedule` VARCHAR(14) NOT NULL,
-  `work_start_date` DATE NOT NULL,
-  `work_end_date` DATE NOT NULL,
+  `program_specialty` VARCHAR(128) NOT NULL,
+  `status` ENUM('Active', 'Inactive') NOT NULL,
   PRIMARY KEY (`trainer_id`),
   UNIQUE INDEX `phone_number_UNIQUE` (`phone_number` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `dbgym`.`memberships`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbgym`.`memberships` (
-  `membership_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `member_id` INT UNSIGNED NOT NULL,
-  `membership_type_id` INT UNSIGNED NOT NULL,
-  `trainer_id` INT UNSIGNED NOT NULL,
-  `membership_start_date` DATE NOT NULL,
-  `membership_end_date` DATE NOT NULL,
-  PRIMARY KEY (`membership_id`),
-  INDEX `fk_memberships_members1_idx` (`member_id` ASC) VISIBLE,
-  INDEX `fk_memberships_membership_types1_idx` (`membership_type_id` ASC) VISIBLE,
-  INDEX `fk_memberships_trainers1_idx` (`trainer_id` ASC) VISIBLE,
-  CONSTRAINT `fk_memberships_members1`
-    FOREIGN KEY (`member_id`)
-    REFERENCES `dbgym`.`members` (`member_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_memberships_membership_types1`
-    FOREIGN KEY (`membership_type_id`)
-    REFERENCES `dbgym`.`membership_types` (`membership_type_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_memberships_trainers1`
-    FOREIGN KEY (`trainer_id`)
-    REFERENCES `dbgym`.`trainers` (`trainer_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
@@ -105,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `dbgym`.`products` (
   `product_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `product_brand` VARCHAR(45) NOT NULL,
   `product_name` VARCHAR(45) NOT NULL,
-  `product_description` VARCHAR(45) NULL,
+  `product_description` VARCHAR(128) NULL,
   `product_price` DECIMAL(10,2) UNSIGNED NOT NULL,
   `available_quantity` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`product_id`))
@@ -113,27 +105,27 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `dbgym`.`transactions`
+-- Table `dbgym`.`product_purchases`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbgym`.`transactions` (
-  `transaction_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `dbgym`.`product_purchases` (
+  `product_purchase_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `member_id` INT UNSIGNED NOT NULL,
   `product_id` INT UNSIGNED NOT NULL,
   `quantity_sold` INT UNSIGNED NOT NULL,
   `transaction_date` DATE NOT NULL,
-  PRIMARY KEY (`transaction_id`),
-  INDEX `fk_transactions_members1_idx` (`member_id` ASC) VISIBLE,
-  INDEX `fk_transactions_products1_idx` (`product_id` ASC) VISIBLE,
-  CONSTRAINT `fk_transactions_members1`
-    FOREIGN KEY (`member_id`)
-    REFERENCES `dbgym`.`members` (`member_id`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_transactions_products1`
+  PRIMARY KEY (`product_purchase_id`),
+  INDEX `fk_product_purchases_products1_idx` (`product_id` ASC) VISIBLE,
+  INDEX `fk_product_purchases_members1_idx` (`member_id` ASC) VISIBLE,
+  CONSTRAINT `fk_product_purchases_products1`
     FOREIGN KEY (`product_id`)
     REFERENCES `dbgym`.`products` (`product_id`)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE)
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_product_purchases_members1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `dbgym`.`members` (`member_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -143,29 +135,83 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `dbgym`.`amenities` (
   `amenity_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `amenity_name` VARCHAR(45) NOT NULL,
+  `walk_in_price` DECIMAL(10,2) UNSIGNED NOT NULL,
+  `time_open` DATETIME NOT NULL,
+  `time_close` DATETIME NOT NULL,
+  `status` ENUM('Active', 'Inactive') NOT NULL,
   PRIMARY KEY (`amenity_id`),
   UNIQUE INDEX `amenity_name_UNIQUE` (`amenity_name` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `dbgym`.`membership_type_amenities`
+-- Table `dbgym`.`subscription_type_amenities`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbgym`.`membership_type_amenities` (
-  `membership_type_id` INT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `dbgym`.`subscription_type_amenities` (
+  `subscription_type_id` INT UNSIGNED NOT NULL,
   `amenity_id` INT UNSIGNED NOT NULL,
-  INDEX `fk_membership_type_amenities_membership_types1_idx` (`membership_type_id` ASC) VISIBLE,
+  INDEX `fk_membership_type_amenities_membership_types1_idx` (`subscription_type_id` ASC) VISIBLE,
   INDEX `fk_membership_type_amenities_amenities1_idx` (`amenity_id` ASC) VISIBLE,
-  CONSTRAINT `fk_membership_type_amenities_membership_types1`
-    FOREIGN KEY (`membership_type_id`)
-    REFERENCES `dbgym`.`membership_types` (`membership_type_id`)
+  CONSTRAINT `fk_subscription_type_amenities_subscription_types1`
+    FOREIGN KEY (`subscription_type_id`)
+    REFERENCES `dbgym`.`subscription_types` (`subscription_type_id`)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_membership_type_amenities_amenities1`
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_subscription_type_amenities_amenities1`
     FOREIGN KEY (`amenity_id`)
     REFERENCES `dbgym`.`amenities` (`amenity_id`)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE)
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbgym`.`amenity_logs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbgym`.`amenity_logs` (
+  `amenity_log_id` INT NOT NULL,
+  `member_id` INT UNSIGNED NOT NULL,
+  `amenity_id` INT UNSIGNED NOT NULL,
+  `usage_start_datetime` DATETIME NOT NULL,
+  `usage_end_datetime` DATETIME NOT NULL,
+  PRIMARY KEY (`amenity_log_id`),
+  INDEX `fk_amenity_logs_members1_idx` (`member_id` ASC) VISIBLE,
+  INDEX `fk_amenity_logs_amenities1_idx` (`amenity_id` ASC) VISIBLE,
+  CONSTRAINT `fk_amenity_logs_members1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `dbgym`.`members` (`member_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_amenity_logs_amenities1`
+    FOREIGN KEY (`amenity_id`)
+    REFERENCES `dbgym`.`amenities` (`amenity_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dbgym`.`training_sessions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dbgym`.`training_sessions` (
+  `training_session_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `subscription_id` INT UNSIGNED NOT NULL,
+  `trainer_id` INT UNSIGNED NOT NULL,
+  `session_start_datetime` DATETIME NOT NULL,
+  `session_end_datetime` DATETIME NOT NULL,
+  PRIMARY KEY (`training_session_id`),
+  INDEX `fk_training_sessions_trainers1_idx` (`trainer_id` ASC) VISIBLE,
+  INDEX `fk_training_sessions_subscriptions1_idx` (`subscription_id` ASC) VISIBLE,
+  CONSTRAINT `fk_training_sessions_trainers1`
+    FOREIGN KEY (`trainer_id`)
+    REFERENCES `dbgym`.`trainers` (`trainer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_training_sessions_subscriptions1`
+    FOREIGN KEY (`subscription_id`)
+    REFERENCES `dbgym`.`subscriptions` (`subscription_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
