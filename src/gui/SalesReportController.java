95123962966e7568b17b1ffe00ec4209e821647e
Lucas Tujan <lucastujan515@gmail.com>
2024-11-23 17:59:50 +0800

    Finished AmenitiesReport and will update SalesReport when done

--- src/gui/SalesReportController.java
+++ src/gui/SalesReportController.java
@@ -3,28 +3,33 @@ import java.awt.event.ActionListener;
 
 public class SalesReportController {
     private final SalesReportView sView = new SalesReportView();
+    private final ProductsDAO pDAO = new ProductsDAO();
 
     public SalesReportController() {
         this.sView.salesReportBackButton(new ActionListener(){
-           @Override
-           public void actionPerformed(ActionEvent e) {
-               MainMenuController mainMenuController = new MainMenuController();
-               sView.getSalesReportFrame().dispose();
-           }
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainMenuController();
+                sView.getSalesReportFrame().dispose();
+            }
         });
 
         this.sView.goToBestSellingButton(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
+                //sView.setBestSellingTable(pDAO.selectProductsByYearlyQuantitySold());
+
                 sView.getBestSellingFrame().setVisible(true);
                 sView.getSalesReportFrame().dispose();
-
             }
         });
 
         this.sView.goToSalesButton(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
+                // TODO maybe this isn't finalized yet
+                //sView.setSalesTable();
+
                 sView.getSalesFrame().setVisible(true);
                 sView.getSalesReportFrame().dispose();
             }
