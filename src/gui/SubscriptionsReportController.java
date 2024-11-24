d1b859b583a8680ce642167347cd31d53adb3175
Diane <marxandreadiane@gmail.com>
2024-11-24 16:05:59 +0800

    sub reports

--- src/gui/SubscriptionsReportController.java
+++ src/gui/SubscriptionsReportController.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionListener;
 
 public class SubscriptionsReportController {
     private final SubscriptionsReportView sView = new SubscriptionsReportView();
+    private SubscriptionsDAO dao = new SubscriptionsDAO();
 
     public SubscriptionsReportController() {
         this.sView.subscriptionsReportBackButton(new ActionListener() {
@@ -18,6 +19,7 @@ public class SubscriptionsReportController {
             public void actionPerformed(ActionEvent e) {
                 sView.getSubscriptionsReportFrame().dispose();
                 sView.getNewMonthlyFrame().setVisible(true);
+                sView.setNewMonthlySubscriptionsTable(dao.getTotalSubsRevenuePerSubTypePerMonthPerYear());
             }
         });
 
@@ -26,6 +28,7 @@ public class SubscriptionsReportController {
             public void actionPerformed(ActionEvent e){
                 sView.getSubscriptionsReportFrame().dispose();
                 sView.getNewYearlyFrame().setVisible(true);
+                sView.setNewYearlySubscriptionsTable(dao.getTotalSubsRevenuePerSubTypePerYear());
             }
         });
 
@@ -34,6 +37,7 @@ public class SubscriptionsReportController {
             public void actionPerformed(ActionEvent e){
                 sView.getSubscriptionsReportFrame().dispose();
                 sView.getTotalSubscriptionsFrame().setVisible(true);
+                sView.setTotalSubscriptionsTable(dao.getTotalSubsRevenuePerSubTypeLifetime());
             }
         });
 
